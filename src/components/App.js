dd0c95f7f5c0dad31e8cb2d8277ec009055143b5
sudev <sudev1997@gmail.com>
2026-05-14 13:04:42 +0530

    fix issue

--- src/components/App.js
+++ src/components/App.js
@@ -1,23 +1,24 @@
-
 import React, { useState } from "react";
 import './../styles/App.css';
 import Login from "./Login";
 
 function App() {
-
   const [isLoggedIn, setIsLoggedIn] = useState(false);
+  
   const handleLogin = () => {
     setIsLoggedIn(true);
   };
 
-
   return (
     <div>
-      {/* Do not remove the main div */}
-      <h1>lift state up</h1>
-      {isLoggedIn ? <h2>Welcome back!</h2> : <Login isLoggedIn={isLoggedIn} onLogin={handleLogin} />}
+      <h1>Parent Component</h1>
+      {isLoggedIn ? (
+        <h2>You are logged in!</h2> 
+      ) : (
+        <Login isLoggedIn={isLoggedIn} onLogin={handleLogin} />
+      )}
     </div>
   )
 }
 
-export default App
+export default App;
--- src/components/Login.js
+++ src/components/Login.js
@@ -1,26 +1,22 @@
-
-import React, { useState } from 'react';
+import React from 'react';
 
 function Login({ isLoggedIn, onLogin }) {
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    onLogin();
+  }
 
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        onLogin();
-    }
-
-
-    return (
-        <div>
-            <h3>Please login</h3>
-            <form onSubmit={handleSubmit}>
-                <input type="text" placeholder="Username" required />
-                <br />
-                <input type="password" placeholder="Password" required />
-                <br />
-                <button type="submit">Login</button>
-            </form>
-        </div>
-    )
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <input type="text" placeholder="Username" required />
+        <br />
+        <input type="password" placeholder="Password" required />
+        <br />
+        <button type="submit">Login</button>
+      </form>
+    </div>
+  )
 }
 
 export default Login;
